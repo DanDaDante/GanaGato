2f2208ff136901ceb742ce993f71b61dc6ecd505
DanDaDante <101757805+DanDaDante@users.noreply.github.com>
2022-05-16 14:49:56 -0500

    Add files via upload

---
+++ ganagato.sql
@@ -0,0 +1,16 @@
+USE Ejem01;
+
+CREATE TABLE ganagato (
+    NProducto tinyint NOT NULL,
+    Concurso int AUTO_INCREMENT PRIMARY KEY,
+    F1 tinyint NOT NULL,
+    F2 tinyint NOT NULL,
+    F3 tinyint NOT NULL,
+    F4 tinyint NOT NULL,
+    F5 tinyint NOT NULL,
+    F6 tinyint NOT NULL,
+    F7 tinyint NOT NULL,
+    F8 tinyint NOT NULL,
+    Bolsa decimal(13,4) NOT NULL,
+    Fecha date NOT NULL
+)ENGINE=InnoDB;
